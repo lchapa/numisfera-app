9ec9c0ffba088fbaa46931d947608692755b63b1
Luis Chapa <luischapam@hotmail.com>
2026-04-06 16:11:26 -0600

    chore(web3): Install dotenv and configure Sepolia testnet environment variables

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,7 @@
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
@@ -12,6 +15,12 @@ export default {
     ganache: {
       url: "http://127.0.0.1:7545",
       chainId: 1337 // Default for Ganache UI
+    },
+    // The public Testnet network (No real money involved)
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "", // Alchemy or Infura URL
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
     }
   }
 };
